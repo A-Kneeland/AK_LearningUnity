4d5019af66ea41140d2092819b592fffa16c6369
AlexanderK <alexanderkn@DESKTOP-TLUR4H1.localdomain>
2020-06-14 15:57:53 -0400

    Finished tutorial 3 "Mathmatical Surfaces" of the catlike coding tutorial. Also finished the notes on it contained in the docx.

---
+++ Mathematical Surfaces/Assets/GraphFunctionName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GraphFunctionName
+{
+    Sine, 
+    MultiSine,
+    Sine2DFunction,
+    MultiSine2DFunction, 
+    Ripple,
+    Cylinder,
+    Sphere,
+    Torus
+}
