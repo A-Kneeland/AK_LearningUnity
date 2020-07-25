43f7a9b2cc7871fa30ca362d527f5da881b065e8
AlexanderK <alexanderkn@DESKTOP-TLUR4H1.localdomain>
2020-07-25 14:38:20 -0400

    Finished the catlike coding Unity tutorial "Sliding A Sphere" where user imput was taken to slide a sphere on a plane and bounce off boundries.

---
+++ Sliding a Sphere/Assets/Scripts/MovingSphere.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovingSphere : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+
+    [SerializeField]
+    Rect allowedArea = new Rect(-4.5f, -4.5f, 9f, 9f);
+
+    [SerializeField, Range(0f, 1f)]
+    float bounciness = 0.5f;
+
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        //transform.localPosition = new Vector3(playerInput.x, transform.localPosition.y, playerInput.y);
+
+        //Vector3 acceleration = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        //velocity += acceleration * Time.deltaTime;
+        //Vector3 displacement = Time.deltaTime * velocity;
+        //transform.localPosition += displacement;
+
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        Vector3 displacement = Time.deltaTime * velocity;
+        Vector3 newPosition = transform.localPosition + displacement;
+        if(!allowedArea.Contains(new Vector2(newPosition.x, newPosition.z)))
+        {
+            //Stick to walls
+            //newPosition.x = Mathf.Clamp(newPosition.x, allowedArea.xMin, allowedArea.xMax);
+            //newPosition.z = Mathf.Clamp(newPosition.z, allowedArea.yMin, allowedArea.yMax);
+
+            if (newPosition.x < allowedArea.xMin)
+            {
+                newPosition.x = allowedArea.xMin;
+                velocity.x *= -1;
+            }
+            else if (newPosition.x > allowedArea.xMax)
+            {
+                newPosition.x = allowedArea.xMax;
+                velocity.x *= -1;
+            }
+
+            if (newPosition.z < allowedArea.yMin)
+            {
+                newPosition.z = allowedArea.yMin;
+                velocity.z *= -1 * bounciness;
+            }
+            else if (newPosition.z > allowedArea.yMax)
+            {
+                newPosition.z = allowedArea.yMax;
+                velocity.z *= -1 * bounciness;
+            }
+        }
+        transform.localPosition = newPosition;
+    }
+}
